3bc3a09853b77ecd98cde5f9a22f4f7da7957464
GhzGarage <qbcore@programmer.net>
2021-04-08 22:18:29 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Doorlock'
+version '1.0.0'
+
+
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
