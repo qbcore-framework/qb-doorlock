03195ff94872fb66ffae36ee2c90079cc241d9bc
GhzGarage <qbcore@programmer.net>
2021-04-08 23:04:35 -0500

    Wait

--- client/main.lua
+++ client/main.lua
@@ -3,7 +3,7 @@ QBCore = nil
 Citizen.CreateThread(function()
 	while QBCore == nil do
 		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-		Citizen.Wait(0)
+		Citizen.Wait(200)
 	end
 end)
 
