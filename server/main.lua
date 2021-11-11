ed8a499b642056b9af09264b0fe57b6d3b19af97
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-11 12:34:17 +0200

    refactor event

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,8 @@
-RegisterServerEvent('qb-doorlock:server:setupDoors')
-AddEventHandler('qb-doorlock:server:setupDoors', function()
+RegisterNetEvent('qb-doorlock:server:setupDoors', function()
 	TriggerClientEvent("qb-doorlock:client:setDoors", QB.Doors)
 end)
 
-RegisterServerEvent('qb-doorlock:server:updateState')
-AddEventHandler('qb-doorlock:server:updateState', function(doorID, state)
+RegisterNetEvent('qb-doorlock:server:updateState', function(doorID, state)
 	QB.Doors[doorID].locked = state
-
 	TriggerClientEvent('qb-doorlock:client:setState', -1, doorID, state)
 end)
