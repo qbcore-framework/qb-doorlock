cbeb96b4f2f7c3dcc900d5e7e3e436fcbd19d41a
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-01-21 19:35:02 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,9 +1,13 @@
 local Translations = {
     error = {
-        not_authorized = "Pas autorisé"
+        not_authorized = "Pas autorisé",
+        lockpick_fail = "Échoué",
+        screwdriverset_not_found = "Vous n'avez pas de kit de tournevis",
+        door_not_locked = "La porte n'est pas vérouillée",
+        door_not_lockpickable = "Cette porte ne peut pas être crochetée"
     },
-    success = {},
-    info = {},
+    success = {
+        lockpick_success = "succès"    },
     general = {
         locking = "~r~verrouillage..",
         unlocking = "~g~déverrouillage..",
