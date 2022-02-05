fedae8c417dedf551b304cba2db2020dfbeae28f
Los823ki <86854130+Los823ki@users.noreply.github.com>
2022-02-06 00:58:30 +0200

    Update fi.lua

--- locales/fi.lua
+++ locales/fi.lua
@@ -1,14 +1,19 @@
 local Translations = {
     error = {
-        not_authorized = "Ei avaimia"
+        not_authorized = "Sinulla ei ole avaimia!",
+        lockpick_fail = "Epäonnistui",
+        screwdriverset_not_found = "Sinulla ei ole ruuvimeisseliä!",
+        door_not_locked = "Ovi ei ole lukossa",
+        door_not_lockpickable = "Ovea ei voi tiirikoida"
     },
-    success = {},
-    info = {},
+    success = {
+        lockpick_success = "Onnistui!"
+    },
     general = {
         locking = "~r~Lukitaan..",
         unlocking = "~g~Avataan..",
         locked = "~r~Lukittu",
-        unlocked = "~g~Avattu",
+        unlocked = "~g~Auki",
         locked_button = "[~g~E~w~] - Lukittu",
         unlocked_button = "[~g~E~w~] - Avattu"
     }
