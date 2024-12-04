a07935df7e2fe378b3a243b665e3c8b49c2a3aec
MrNewb <47620135+MrNewb@users.noreply.github.com>
2024-12-04 03:06:48 -0500

    feat(add exports for external data pulling)
    This pr will allow me to pull data externally.
    Intended use, external scripts pull this data to find the nearby door, check status, etc

--- client/main.lua
+++ client/main.lua
@@ -883,3 +883,19 @@ CreateThread(function()
 		Wait(sleep)
 	end
 end)
+
+exports('GetClosestDoor', function()
+	return closestDoor
+end)
+
+exports('GetNearbyDoors', function()
+	return nearbyDoors
+end)
+
+exports('GetDoorList', function()
+	return Config.DoorList
+end)
+
+exports('GetDoorStates', function()
+	return Config.DoorStates
+end)
